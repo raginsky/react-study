61ce643d8f57fe49073e3593dfc8d03292d44f98
Michael <michelr@proftit.com>
2022-03-23 09:13:01 +0200

    ExpenseDate logic and separate component

--- src/App.js
+++ src/App.js
@@ -28,15 +28,15 @@ function App() {
             <ExpenseItem title={expenses[0].title}
                          date={expenses[0].date}
                          amount={expenses[0].amount}
-            ></ExpenseItem>
+            />
             <ExpenseItem title={expenses[1].title}
                          date={expenses[1].date}
                          amount={expenses[1].amount}
-            ></ExpenseItem>
+            />
             <ExpenseItem title={expenses[2].title}
                          date={expenses[2].date}
                          amount={expenses[2].amount}
-            ></ExpenseItem>
+            />
         </div>
     );
 }
