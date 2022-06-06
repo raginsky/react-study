75202db5803780618e8f1d5684587a15f8500093
Michael <michelr@proftit.com>
2022-06-06 09:03:52 +0300

    started work on chart

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,13 @@
+import ChartBar from './ChartBar';
+import './Chart.csss';
+
+const Chart = props => {
+    return <div className="chart">{props.dataPoints.map(dataPoint => <ChartBar
+        key={dataPoint.label}
+        value={dataPoint.value}
+        maxValue={null}
+        label={dataPoint.label}/>)}
+    </div>;
+};
+
+export default Chart;
