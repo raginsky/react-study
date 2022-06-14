4adeb1c7e4a78dddf692f056f26dba59bbb054c3
Michael <michelr@proftit.com>
2022-06-14 09:04:41 +0300

    new project preparation

---
+++ 00-expenses-chart/src/App.js
@@ -0,0 +1,54 @@
+import {useState} from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+// function App
+
+const exampleData = [
+    {
+        id: 'e1',
+        date: new Date(2021, 2, 17),
+        title: 'Car Insurance',
+        amount: 349.50
+    },
+    {
+        id: 'e2',
+        date: new Date(2019, 2, 18),
+        title: 'New TV',
+        amount: 500.50
+    },
+    {
+        id: 'e3',
+        date: new Date(2022, 2, 20),
+        title: 'Groceries',
+        amount: 85.78
+    },
+];
+
+const App = () => {
+    const [expenses, setExpenses] = useState(exampleData);
+    const addExpenseHandler = expense => {
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses];
+        });
+    };
+
+    /**
+     * An old approach
+     * [type, props, child]
+     * [html-element, object that configures previous element (sets attr), component between opening and closing tags]
+     *
+     return React.createElement('div', {},
+     React.createElement('h2', {}, 'Lets get started!'),
+     React.createElement(Expenses, {items: expenses})
+     );
+     */
+
+    return (
+        <div>
+            <NewExpense onAddExpense={addExpenseHandler}/>
+            <Expenses items={expenses}/>
+        </div>
+    );
+};
+
+export default App;
