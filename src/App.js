71ae92f73ea197f0f3298d2735bc47b92d400cbe
Michael <michelr@proftit.com>
2022-06-16 09:22:56 +0300

    form submit wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
+
 const App = () => {
-    return '';
+    return (
+        <div>
+            <section id="goal-form">
+                <CourseInput onAddData={onAddData} />
+            </section>
+            <section id="goals">
+            </section>
+        </div>
+    );
 };
 
 export default App;
--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './CourseInputcss';
+import React, {useState} from 'react';
+import './CourseInput.css';
 
 const CourseInput = props => {
+    [enteredTitle, setEnteredTitle] = useState('');
+
+    const titleChangeHandler = (e) => {
+        setEnteredTitle(e.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const data = {
+            title: enteredTitle;
+        }
+        props.onAddData(data);
+        setEnteredTitle('');
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="form-control">
-                <label>Couse Goal</label>
-                <input type="text"/>
+                <label>Course Goal</label>
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
             <button>Add Goal</button>
         </form>
